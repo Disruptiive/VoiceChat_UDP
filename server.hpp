d15bf5745468649b6a6e2d538d421432ff72378b
Antonis <atzatzidis@gmail.com>
2024-09-02 23:17:48 +0300

    added multiple client support

--- server.hpp
+++ server.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <deque>
 #include <set>
+#include <unordered_set>
 #include "ringbuffer.hpp"
 #include "constants.hpp"
 
@@ -19,21 +20,12 @@ public:
     Server(boost::asio::io_context& io_context)
         : m_socket(io_context, udp::endpoint(udp::v4(), 6000)), m_strand(io_context) {
         udp::resolver resolver(io_context);
-        my_favourite_ever = *resolver.resolve(udp::v4(), "", "fredo-chat").begin();
         start_receiving();
     }
 
 private:
     static constexpr size_t MAX_PACKET_SIZE = 65507; // Maximum UDP packet size
-    /*
-    struct ClientContext {
-        std::vector<unsigned char> buffer;
-        uint64_t expected_sequence;
-        // Add more client-specific data as needed
-    };
-    
-    std::map<udp::endpoint, ClientContext> m_clients;
-    */
+
     void start_receiving() {
         auto buffer = std::make_shared<std::vector<unsigned char>>(constants::DUMMY_NUM);
         auto endpoint = std::make_shared<udp::endpoint>();
@@ -43,11 +35,14 @@ private:
             *endpoint,
             boost::asio::bind_executor(m_strand,
                 [this, buffer, endpoint](const boost::system::error_code& ec, std::size_t bytes_transferred) {
-                    if (!ec) {
+                    if (!ec){
+                        udp::endpoint new_endpoint((*endpoint).address(), 6001);
+                        endpoints.insert(new_endpoint);
+                        std::cout << "Amount of endpoints: " << endpoints.size() << "\n";
                         handle_receive(buffer, bytes_transferred, *endpoint);
                     }
                     else {
-                        handle_error(ec, "receive");
+                        handle_error(ec, "receive", *endpoint);
                         start_receiving(); // Start receiving next packet immediately
                     }
                 }
@@ -73,7 +68,11 @@ private:
         }
         std::cout << "Received: " << packet->bytes << " with timestamp: " << packet->timestamp << "\n";
 
-        start_sending(buffer,bytes_transferred, my_favourite_ever);
+        for (auto& e : endpoints) {
+            std::cout << "Sending to endpoint: " << e.address().to_string() << "\n";
+            start_sending(buffer, bytes_transferred, e);
+        }
+        
     }
 
     void start_sending(std::shared_ptr<std::vector<unsigned char>> buffer, std::size_t bytes_to_send, const udp::endpoint& target_endpoint) {
@@ -81,10 +80,10 @@ private:
             boost::asio::buffer(*buffer, bytes_to_send),
             target_endpoint,
             boost::asio::bind_executor(m_strand,
-                [this,buffer](const boost::system::error_code& ec, std::size_t bytes_transferred) {
+                [this,buffer, target_endpoint](const boost::system::error_code& ec, std::size_t bytes_transferred) {
                     std::cout << "Sent: " << bytes_transferred << " bytes.\n";
                     if (ec) {
-                        handle_error(ec, "send");
+                        handle_error(ec, "send", target_endpoint);
                     }
                     start_receiving();
                 }
@@ -92,12 +91,14 @@ private:
         );
     }
 
-    void handle_error(const boost::system::error_code& ec, const std::string& operation) {
+    void handle_error(const boost::system::error_code& ec, const std::string& operation, const udp::endpoint& target_endpoint) {
+        udp::endpoint new_endpoint(target_endpoint.address(), 6001);
+        endpoints.erase(new_endpoint);
         std::cout << "Error during " << operation << ": " << ec.message() << "\n";
     }
 
 
-    udp::endpoint my_favourite_ever;
     boost::asio::io_context::strand m_strand;
     udp::socket m_socket;
+    std::set<udp::endpoint> endpoints;
 };
